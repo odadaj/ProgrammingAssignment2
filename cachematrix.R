bded4cd8abcbdeb2c701ad4d0434e8361024705d
odadaj <jacob.odada@gmail.com>
2017-06-23 21:27:36 +0200

    Changed comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Prgramming assignment 2 to test on caching.
+## the function does XXXXXX
 
 ## Write a short comment describing this function
 
